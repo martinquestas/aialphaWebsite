bd8aee58bb9409103e64629a89d04e3bd4dd49ef
Martin Mengo <tinchomengo@gmail.com>
2023-06-14 15:43:10 -0300

    Created carousel

--- main.js
+++ main.js
@@ -129,3 +129,84 @@ Array.prototype.forEach.call(links, function (elem, index) {
     });
   }
 });
+
+//CAROUSEL
+
+const dotsContainer = document.getElementById("dotsContainer");
+const dots = Array.from(dotsContainer.getElementsByClassName("dot"));
+
+function setActiveDot(index) {
+  dots.forEach((dot, dotIndex) => {
+    dot.classList.toggle("active", dotIndex === index);
+  });
+}
+
+let imgObject = [
+  "static/images/why1.png",
+  "static/images/why2.png",
+  "static/images/why3.png",
+  "static/images/why4.png",
+  "static/images/why4.png",
+  "static/images/why4.png",
+];
+
+let mainImg = 0;
+let prevImg = imgObject.length - 1;
+let nextImg = 1;
+
+function loadGallery() {
+  let mainView = document.getElementById("mainView");
+  mainView.style.background = "url(" + imgObject[mainImg] + ")";
+
+  let leftView = document.getElementById("leftView");
+  leftView.style.background = "url(" + imgObject[prevImg] + ")";
+
+  let rightView = document.getElementById("rightView");
+  rightView.style.background = "url(" + imgObject[nextImg] + ")";
+
+  let linkTag = document.getElementById("linkTag");
+  linkTag.href = imgObject[mainImg];
+
+  setActiveDot(mainImg);
+}
+
+dots.forEach((dot, index) => {
+  dot.addEventListener("click", () => handleDotClick(index));
+});
+
+function scrollRight() {
+  prevImg = mainImg;
+  mainImg = nextImg;
+  if (nextImg >= imgObject.length - 1) {
+    nextImg = 0;
+  } else {
+    nextImg++;
+  }
+  loadGallery();
+}
+
+function scrollLeft() {
+  nextImg = mainImg;
+  mainImg = prevImg;
+
+  if (prevImg === 0) {
+    prevImg = imgObject.length - 1;
+  } else {
+    prevImg--;
+  }
+  loadGallery();
+}
+
+document.getElementById("navRight").addEventListener("click", scrollRight);
+document.getElementById("navLeft").addEventListener("click", scrollLeft);
+document.getElementById("rightView").addEventListener("click", scrollRight);
+document.getElementById("leftView").addEventListener("click", scrollLeft);
+document.addEventListener("keyup", function (e) {
+  if (e.keyCode === 37) {
+    scrollLeft();
+  } else if (e.keyCode === 39) {
+    scrollRight();
+  }
+});
+
+loadGallery();
