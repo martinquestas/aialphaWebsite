25c09eb045418619ec14f75b43905e2b5fb73d0e
Martin Mengo <tinchomengo@gmail.com>
2023-07-25 01:30:27 -0300

    Attempt for Auth0

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,14 @@
 var router = require("express").Router();
 const { requiresAuth } = require("express-openid-connect");
 
-router.get("/api/", function (req, res, next) {
-  // Updated route
+router.get("/", function (req, res, next) {
   res.render("index", {
     title: "Auth0 Webapp sample Nodejs",
     isAuthenticated: req.oidc.isAuthenticated(),
   });
 });
 
-router.get("/api/profile", function (req, res, next) {
-  // Updated route
+router.get("/profile", function (req, res, next) {
   res.render("index", { title: "Profile" });
 });
 
